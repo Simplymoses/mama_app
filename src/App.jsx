eb42555dedfa20702185df6d8529b708459e3d11
Moses Ng <simplymoses@gmail.com>
2023-08-24 23:36:53 +0800

    Start of TW Training
    Commit to moses branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,14 +9,14 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="h-24 p-6 hover:shadow" alt="Special Momo Logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className='moses'>Vite + React</h1>
-      <div className="card">
+      <h1 className='text-purple-500'>Vite + React</h1>
+      <div className="p-8 text-red-500">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
