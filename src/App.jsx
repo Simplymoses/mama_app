6b164f6ffa4d1b2cefafae6dae9f51489f2ddcf7
Moses Ng <simplymoses@gmail.com>
2023-08-27 18:41:40 +0800

    Basic structures of sidebar and tn

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import Topbar from './components/Topbar'
 const App = () => {
   return ( 
     <>
-
+      <div className="flex space-x-2 min-h-screen text-white Parent bg-slate-900">
+        <div className="container w-2/12 bg-red-500 text text-center rounded-r-lg">Sidebar</div>
+        <div className="container w-10/12 bg-orange-500 h-16 max-w-full text-center rounded-l-lg">Topnav</div>
+      </div>
     </>
   )
 }
