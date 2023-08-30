28cea7a9d8d6ae9ea5b2968a761f06b92b2f0730
Moses Ng <simplymoses@gmail.com>
2023-08-30 07:08:01 +0000

    Sidebar Rework with JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
 import { Container } from 'postcss'
-import Sidebar from './components/Sidebar'
-import Topbar from './components/Topbar'
+import { useState } from 'react';
+import { BsArrowLeftShort } from "react-icons/bs";
+import { BsChatSquareHeartFill } from "react-icons/bs";
 
 const App = () => {
-  return ( 
+  const [open, setOpen] = useState(true);
+  return (
     <>
-      <div className="relative flex min-h-screen text-black Parent bg-slate-900">
-          <div className="absolute flex flex-col w-2/12 w-fit h-fit bg-red-500 text text-center rounded-r-lg px-2 py-2 space-y-2">Sidebar
-            <div className="box-content h-5 w-5 p-4 border-4">Icon</div>
-            <button className="bg-yellow-100 border-1 border-black px-1 py-4 rounded">SB Option 1</button> 
-            <button className="bg-yellow-200 border-1 border-black px-1 py-4 rounded">SB Option 2</button> 
-            <button className="bg-yellow-300 border-1 border-black px-1 py-4 rounded">SB Option 3</button> 
-            <button className="bg-yellow-400 border-1 border-black px-1 py-4 rounded">SB Option 4</button> 
-            <button className="bg-yellow-500 border-1 border-black px-1 py-4 rounded">SB Option 5</button> 
-            <button className="bg-yellow-600 border-1 border-black px-1 py-4 rounded">SB Option 6</button> 
-            <button className="bg-yellow-700 border-1 border-black px-1 py-4 rounded">SB Option 7</button> 
-          </div>
-          <div className="absolute right-0 flex flex-row w-10/12 w-fit h-fit bg-orange-500 h-16 text-center rounded-l-lg px-2 py-2 space-x-2">Topnav
-            <button className="bg-yellow-100 border-1 border-black px-1 py-4 rounded">SB Option 1</button> 
-            <button className="bg-yellow-200 border-1 border-black px-1 py-4 rounded">SB Option 2</button> 
-            <button className="bg-yellow-300 border-1 border-black px-1 py-4 rounded">SB Option 3</button> 
-            <button className="bg-yellow-400 border-1 border-black px-1 py-4 rounded">SB Option 4</button> 
-            <button className="bg-yellow-500 border-1 border-black px-1 py-4 rounded">SB Option 5</button> 
-            <button className="bg-yellow-600 border-1 border-black px-1 py-4 rounded">SB Option 6</button> 
-            <button className="bg-yellow-700 border-1 border-black px-1 py-4 rounded">SB Option 7</button> 
-          </div>
+      <div className="flex">
+        <div 
+          className={`bg-pink-300 h-screen p-5 pt-8 ${
+            open ? "w-72" : "w-20"
+          } duration-300 relative`}
+        >
+          <BsArrowLeftShort className={`bg-white text-green-300 text-3xl rounded-full 
+          absolute -right-3 top-9 border 
+          border-green-300 cursor-pointer ${
+            !open && "rotate-180"
+          }`} 
+          onClick={() => setOpen(!open)} 
+        />
+        <div className="inline-flex">
+          <BsChatSquareHeartFill className="bg-pink-300 text-4xl
+          rounded cursor-pointer block float-left mr-2 pt-1" />
+          <h1 className={`text-white origin-left font-medium
+          text-2xl duration-300 ${!open && "scale-0"}`}>
+            MamaApp
+          </h1>
+        </div>
+          
+        </div>
+
+        <div className="p-7">
+          <h1 className="text-2xl font-semibold">Homescreen</h1>
+        </div>
       </div>
     </>
   )
